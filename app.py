cfb63dfb81160d43df29529cb5864c20f0dcfa6a
PavanSurisetti <pavan1898235@gmail.com>
2025-05-24 12:19:58 +0530

    First Code With Flask

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
-app=Flask(__name__)
+import os
+
+app = Flask(__name__)
+
 @app.route("/")
 def wish():
-    return'<p style="color:Blue">Hello Ganesha</p>'
-if __name__=='__main__':
-    app.run(debug=True)
+    return '<p style="color:Blue">Hello Ganesha</p>'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
